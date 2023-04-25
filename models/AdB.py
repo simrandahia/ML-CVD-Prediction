277e939ea9c2f2c87b9ea5d8544e66ccf134a9a5
Simran <simran@simran.local>
2023-04-25 11:04:03 +0930

    AdB

--- models/AdB.py
+++ models/AdB.py
@@ -3,11 +3,12 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import roc_auc_score
 
 # Define the AdaBoost model
 base_model = DecisionTreeClassifier(max_depth=1)
 n_estimators = 5000
-learning_rate = 0.009
+learning_rate = 0.001
 model = AdaBoostClassifier(estimator=base_model, n_estimators=n_estimators, learning_rate=learning_rate)
 
 data = import_uci()
@@ -22,6 +23,13 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # Train the AdaBoost model
 model.fit(X_train, y_train)
 
+# Predict probabilities for test set
+y_prob = model.predict_proba(X_test)
+
+# Calculate AUC-ROC score
+auc_roc = roc_auc_score(y_test, y_prob, multi_class='ovr')
+
+
 # Evaluate the AdaBoost model
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
@@ -29,3 +37,4 @@ precision = precision_score(y_test, y_pred, average='weighted', zero_division=1)
 recall = recall_score(y_test, y_pred, average='weighted')
 f1 = f1_score(y_test, y_pred, average='weighted')
 print(f"\nAccuracy = {accuracy:.4f}\nPrecision = {precision:.4f}\nRecall = {recall:.4f}\nF1 Score = {f1:.4f}")
+print(f"\nAUC-ROC Score = {auc_roc:.4f}")
